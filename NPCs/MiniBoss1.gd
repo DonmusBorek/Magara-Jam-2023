ca5ff601b0c719afcc9001fb39dc9cfc1c9ee464
MaviTurnusol <betatorium@gmail.com>
2023-11-17 02:09:28 +0300

    mini pushy

---
+++ NPCs/MiniBoss1.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var state = "alive"
+var speed = 50
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var turned = false
+var playerinsight
+
+func _physics_process(delta):
+	if(state == "alive"):
+		if(State.player.global_position.x < position.x):
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "speed", -300, 2)
+		else:
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "speed", +300, 2)
+		velocity.x = speed
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if(sign(velocity.x) < 0):
+		$marker.scale.x = 1
+	else:
+		$marker.scale.x = -1
+		
+	if(!playerinsight && $marker/Ust.frame == 7):
+		$marker/Ust.stop()
+		
+	move_and_slide()
+
+func _on_detector_body_entered(body):
+	if(body == State.player):
+		$marker/Ust.play("default")
+		playerinsight = true
+	pass # Replace with function body.
+
+func _on_detector_body_exited(body):
+	if(body == State.player):
+		playerinsight = false
+	pass # Replace with function body.
--- state.gd
+++ state.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var player
+var WalkComp = true
+var VisionComp = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
