5e484e4d923a204ec6bfaf4356c6daeebcb38cea
DonmusBorek <109541782+DonmusBorek@users.noreply.github.com>
2023-11-17 01:57:49 +0300

    arsenal

--- scripts/drop_place.gd
+++ scripts/drop_place.gd
@@ -3,12 +3,10 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	modulate = Color(Color.GRAY, 0.7)
+	modulate = Color(Color.ORANGE, 0.7)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if GlobalDrag.is_dragging:
-		visible = false
-	else :
 		visible = true
--- scripts/object.gd
+++ scripts/object.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+@export var nagme:String
+@export var sprite:Texture2D
+
 var draggable = false
 var is_inside_dropable = false
 var body_ref
 var offset: Vector2
 var initialPos: Vector2
+
+func _ready():
+	$Sprite2D.texture = sprite
 
 func _process(delta):
 	if draggable:
@@ -12,8 +18,15 @@ func _process(delta):
 			initialPos = global_position
 			offset = get_global_mouse_position() - global_position
 			GlobalDrag.is_dragging = true
-		if Input.is_action_just_pressed("Left_Click"):
+		if Input.is_action_pressed("Left_Click"):
 			global_position = get_global_mouse_position() - offset
+		elif Input.is_action_just_released("Left_Click"):
+			GlobalDrag.is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_dropable:
+				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
 	
 
 
@@ -32,10 +45,10 @@ func _on_area_2d_mouse_exited():
 func _on_area_2d_body_entered(body:StaticBody2D):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = true
-		body.modulate = Color(Color.DIM_GRAY,1)
+		body.modulate = Color(Color.DARK_GOLDENROD,1)
 		body_ref = body
 		
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = false
-		modulate = Color(Color.GRAY, 0.7)
+		body.modulate = Color(Color.ORANGE, 0.7)
