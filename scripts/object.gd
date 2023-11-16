0ef0ed1104263e145a471d89efb9c24e87ea7b9a
DonmusBorek <109541782+DonmusBorek@users.noreply.github.com>
2023-11-17 01:00:35 +0300

    ad

--- scripts/object.gd
+++ scripts/object.gd
@@ -10,16 +10,24 @@ func _process(delta):
 
 
 func _on_area_2d_mouse_entered():
-	pass # Replace with function body.
+	if not GlobalDrag.is_dragging:
+		draggable = true
+		scale = Vector2(1.05,1.05)
 
 
 func _on_area_2d_mouse_exited():
-	pass # Replace with function body.
+	if not GlobalDrag.is_dragging:
+		draggable = false
+		scale = Vector2(1,1)
 
 
 func _on_area_2d_body_entered(body:StaticBody2D):
-	pass # Replace with function body.
-
-
+	if body.is_in_group("dropable"):
+		is_inside_dropable = true
+		body.modulate = Color(Color.DIM_GRAY,1)
+		body_ref = body
+		
 func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
+	if body.is_in_group("dropable"):
+		is_inside_dropable = false
+		modulate = Color(Color.GRAY, 0.7)
