4891289496637619ff69d1eae20ca53b7a1848c8
DonmusBorek <109541782+DonmusBorek@users.noreply.github.com>
2023-11-16 18:54:53 +0300

    menuilkhal

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -64,8 +64,17 @@ func _on_geri_toggled(button_pressed):
 		timer.start()
 		
 func _on_master_2_value_changed(value):
-	volume(0,value/2)
+	volume(0,value)
 
 func volume(bus_index, value):
+	
 	AudioServer.set_bus_volume_db(bus_index,value)
 	
+
+
+func _on_music_value_changed(value):
+	volume(2,value)
+
+
+func _on_sfx_value_changed(value):
+	volume(1,value)
