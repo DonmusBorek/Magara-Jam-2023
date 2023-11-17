fabc1a166d68e8373359cf7b069b9c92c4797a76
MaviTurnusol <betatorium@gmail.com>
2023-11-17 18:58:26 +0300

    enemy shooter

--- gun_1.gd
+++ gun_1.gd
@@ -28,18 +28,28 @@ func _physics_process(delta):
 			var sfx = preload("res://scenes/gunshotsfx.tscn").instantiate()
 			add_child(sfx)
 			
+			#Shot
+			$Marker2D/Shot.visible = true
+			
 			#Hitscan
 			if($Marker2D/guncast.is_colliding()):
+				var hitenemy = preload("res://hit_enemy.tscn").instantiate()
+				hitenemy.global_position = $Marker2D/guncast.get_collision_point()
+				get_tree().root.add_child(hitenemy)
 				if($Marker2D/guncast.get_collider().is_in_group("Enemy")):
 					$Marker2D/guncast.get_collider().health -= 1
-					var hitenemy = preload("res://hit_enemy.tscn").instantiate()
-					hitenemy.global_position = $Marker2D/guncast.get_collision_point()
-					get_tree().root.add_child(hitenemy)
+					$Marker2D/guncast.get_collider().get_node("HitFlash").play("HitFlash")
+					$Marker2D/guncast.get_collider().velocity -= (State.player.global_position-$Marker2D/guncast.get_collider().global_position).normalized() * 800
+					var kivilcim = preload("res://scenes/kivilcim.tscn").instantiate()
+					kivilcim.rotation_degrees = $Marker2D.rotation_degrees
+					kivilcim.global_position = $Marker2D/guncast.get_collision_point()
+					get_tree().root.add_child(kivilcim)
 					print("shit")
-			
 			#cooldown
 			cooldown = true
 			$cd.start()
+		else:
+			$Marker2D/Shot.visible = false
 func _on_cd_timeout():
 	cooldown = false
 	pass # Replace with function body.
